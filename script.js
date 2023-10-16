a08f0cc4d2551c03b5210fc5b32d8c132d6b5025
Calebe <caleberl@outlook.com>
2023-10-16 14:29:31 -0300

    fix image and email

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 const form = document.querySelector("form")
+const input = document.querySelector("#input")
 const signUpPage = document.querySelector(".wrapper")
-const sucessMassage = document.querySelector(".sucess-massage")
+const sucessMessage = document.querySelector(".sucess-message")
 const buttonDismiss = document.querySelector(".button-dismiss")
+const confirmationEmail = document.querySelector(".confirmation-email")
+
+
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
     signUpPage.classList.toggle("hide")
-    sucessMassage.classList.toggle("hide")
+    sucessMessage.classList.toggle("hide")
 
+    confirmationEmail.innerHTML = input.value
 })
 
 buttonDismiss.addEventListener("click", (e) => {
     e.preventDefault()
     signUpPage.classList.toggle("hide")
-    sucessMassage.classList.toggle("hide")
+    sucessMessage.classList.toggle("hide")
 })
